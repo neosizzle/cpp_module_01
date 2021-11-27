b91752eab34beee98dcd8e72b93417f0bd241fed
neosizzle <junahnngd1115@gmail.com>
2021-11-27 19:29:10 +0800

    null weapon ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -7,6 +7,7 @@
 */
 HumanB::HumanB(std::string name) : _name(name)
 {
+	_weapon = 0;
 }
 
 HumanB::~HumanB()
@@ -24,8 +25,8 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack()
 {
-	if ("" == _weapon->getType())
-		std::cout << _name << "	Cant attack without a weapon" << "\n";
-	else
+	if (_weapon)
 		std::cout << _name << "  attacks with his " << _weapon->getType() << "\n";
+	else
+		std::cout << _name << "	Cant attack without a weapon" << "\n";
 }
